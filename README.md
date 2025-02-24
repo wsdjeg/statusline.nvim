d1d38326a95220fa56ed8ce57556f0b3ab77c726
Eric Wong <wsdjeg@outlook.com>
2025-02-25 00:18:45 +0800

    init version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # statusline.nvim
+
+`statusline.nvim` is a simple statusline plugin for neovim.
+
+
+## Install
+
+
+## Config
+
+```lua
+
+require('statusline').setup({
+  left_sections = {'winnr', 'filename'},
+  right_sections = {'fileformat', 'cursorpos'},
+}}
+
+```
---
+++ lua/statusline/config.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+local default = {
+  left_sections = {'winnr', 'filename'},
+  right_sections = {'fileformat', 'cursorpos'},
+}
+
+function M.setup(opt)
+  default = vim.tbl_deep_extend('force', default, opt)
+end
+
+function M.get()
+  return default
+end
+
+return M
